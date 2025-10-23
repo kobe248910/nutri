22a2a517c13ad00519bd403c858ba7716158dd99
kobe248910 <joao.ambrosio.cadena@escola.pr.gov.br>
2025-10-23 08:50:54 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -1,45 +1,47 @@
-var  botaoAdicionar = document.querySelector("#adicionar-paciente");
-//execulta os codigos ao clicar no botão 
-botaoAdicionar.addEventListener ('click',function(event){
-    event.preventDefault();
+//acessa o botao
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+//executa o codigo ao acionar o botao
+botaoAdicionar.addEventListener('click', function(event){
+event.preventDefault();
 
-
-//acessa o formúlario 
+//acessa o formulario
 var formulario = document.querySelector("#form-adiciona");
-console.log(formulario);
-
-//captura os valores digitados 
-var nome = formulario.nome.value;
-var peso = formulario.peso.value;
-var altura = formulario.altura.value;
-var gordura = formulario.gordura.value;
-
-var pacienteTr = document.createElement("tr");
-
-//cria as tags <td>
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
-
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calcularIMC(peso,altura)
-
-
-//adicionar as tags na tela de usuario
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(imcTd);
-
-//variavel para acessar tdBody e criar a tabela
+var paciente = buscaValoresFormulario(formulario)
+var pacienteTr = criarTr(paciente);
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.appendChild(pacienteTr);
-
-
 });
+
+function buscaValoresFormulario(formulario){
+// Captura os valores digitados
+var paciente = {
+    
+    nome: formulario.nome.value,
+    peso: formulario.peso.value,
+    altura: formulario.altura.value,
+    gordura: formulario.gordura.value,
+    imc: calcularIMC(formulario.peso.value, formulario.altura.value)
+}
+return paciente;
+}
+
+function criarTr(paciente){
+//Cria as tas <tr>
+var pacienteTr = document.createElement("tr");
+pacienteTr.classList.add("paciente");
+
+//aiciona o conteudo que adicionou no formulario para mostrar ao usuário
+pacienteTr.appendChild(criarTd(paciente.nome, "info-nome"));
+pacienteTr.appendChild(criarTd(paciente.peso, "info-peso"));
+pacienteTr.appendChild(criarTd(paciente.altura, "info-altura"));
+pacienteTr.appendChild(criarTd(paciente.gordura, "info-gordura"));
+pacienteTr.appendChild(criarTd(paciente.imc, "info-imc"));
+return pacienteTr
+}
+
+function criarTd(dado, classe){
+const td = document.createElement("td");
+td.textContent = dado;
+td.classList.add("classe");
+return td;
+}
